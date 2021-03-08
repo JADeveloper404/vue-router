a478f3a94ccaf41f6b23b91b5bf473431f85eec5
JADeveloper <jesusdeveloper404@gmail.com>
2021-03-08 16:20:49 -0500

    Proyecto terminado

---
+++ src/views/Articulo.vue
@@ -0,0 +1,33 @@
+<template>
+  <Titulo :texto="post.id + ' - ' + post.title" />
+  <p>{{ post.body }}</p>
+</template>
+
+<script>
+import Titulo from '@/components/Titulo'
+
+export default {
+    components: {
+        Titulo
+    },
+    data() {
+        return {
+            post: {}
+        }
+    },
+    methods: {
+        async obtenerArticulo(id) {
+            const data = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+            const obj = await data.json();
+            this.post = obj;
+        }
+    },
+    created() {
+        this.obtenerArticulo(this.$route.params.id);
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,10 +1,37 @@
 <template>
-  blog
+    <Titulo texto="Titulo de mi blog" />
+    <div v-for="(post) in arrayBlog" :key="post.id">
+        <router-link :to="`/blog/${ post.id }`">{{ post.title }}</router-link>
+    </div>
 </template>
 
 <script>
-export default {
 
+import Titulo from '@/components/Titulo'
+
+export default {
+    components: {
+        Titulo
+    },
+    data() {
+        return {
+            arrayBlog: []
+        }
+    },
+    methods: {
+        async consumirApi() {
+            try {
+                const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+                const array = await data.json()
+                this.arrayBlog = array;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    },
+    created() {
+        this.consumirApi()
+    }
 }
 </script>
 
